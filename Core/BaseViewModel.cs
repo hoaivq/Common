1a4fb1ed779f488a212b87fb855aa7373dd1cb01
hoaivq <admin@HOAIVQ>
2023-02-06 09:38:10 +0700

    Add project files.

---
+++ Core/BaseViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Common
+{
+    public class BaseViewModel<T, TModel> : CoreViewModel where T: IView where TModel : BaseModel, new()
+    {
+        public T _View;
+        public TModel _Model { get; set; } = new TModel();
+
+        
+        public ICommand CloseViewCommand { get; set; }
+
+        public BaseViewModel(T View)
+        {
+            _View = View;
+
+            CloseViewCommand = new RelayCommand<object>((p) => { return true; }, async (p) =>
+            {
+                _View.CloseView();
+                await Task.CompletedTask;
+            });
+        }
+    }
+}
---
+++ Core/CoreViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Common
+{
+    public class CoreViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public ICommand LoadViewCommand { get; set; }
+        public CoreViewModel()
+        {
+            LoadViewCommand = new RelayCommand<object>((p) => { return true; }, async (p) =>
+            {
+                await LoadView();
+            });
+        }
+
+        public virtual async Task LoadDanhMuc()
+        {
+
+            await Task.CompletedTask;
+        }
+        public virtual async Task LoadView()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
